1ecd606fd3e346365aa2834af5f3807ae3d23361
Akinbobola Olawole <stolaksoftech@yahoo.com>
2024-02-15 19:00:13 +0100

    optimsation

--- app/Http/Controllers/AccountReport.php
+++ app/Http/Controllers/AccountReport.php
@@ -408,7 +408,6 @@ class AccountReport extends Controller
     public function Transaction_Summary(Request $request)
     {
         //if (!$this->AuthenticateRoute("new-brand")) return view('lock.index');
-<<<<<<< Updated upstream
         $data['ref']=$request->input('ref');
        	$data['fromdate']=$request->input('fromdate');
        	$data['todate']=$request->input('todate');
@@ -432,30 +431,6 @@ class AccountReport extends Controller
         }
         $data['RefBatch']= AccountTrait::refBatch();
         $data['Trans_Summary'] = AccountTrait::trans_Summary($data['fromdate'],$data['todate'],$data['ref']);
-=======
-        $data['ref'] = $request->input('ref');
-        $data['fromdate'] = $request->input('fromdate');
-        $data['todate'] = $request->input('todate');
-        if ($data['todate'] == "") {$data['todate'] = date("Y-m-d");}
-        if ($data['fromdate'] == "") {$data['fromdate'] = date("Y-m-d");}
-        if (isset($_POST['del'])) {
-
-            $del = $request->input('deleteid');
-            $trans_date = db::table('account_transactions')->where('ref', $del)->value('transdate');
-
-            if (db::table('tblfinancial_end')->where('year_end_date', '>=', $trans_date)->first()) {
-                return back()->with('error_message', ' This year perid has already been closed. Hence the transaction not deletable');
-            }
-
-            DB::delete("DELETE FROM `account_transactions` WHERE `ref`='$del'");
-            DB::delete("DELETE FROM `tblbatch_post_temp` WHERE `ref`='$del'");
-            DB::delete("DELETE FROM `pettyhandling_transactions` WHERE `ref`='$del'");
-            DB::delete("DELETE FROM `temp_journal_transfer` WHERE `ref`='$del'");
-            return back()->with('message', ' Record successfully trashed.');
-        }
-        $data['RefBatch'] = AccountTrait::refBatch();
-        $data['Trans_Summary'] = AccountTrait::trans_Summary($data['fromdate'], $data['todate'], $data['ref']);
->>>>>>> Stashed changes
 
         // dd($data);
         return view('AccountReport.transaction_summary', $data);
